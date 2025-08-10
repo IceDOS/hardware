be9d078ca5fefaaa58bf53ee5fd15387537dd7dd
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-11 01:02:29 +0300

    feat(modules): add grub bootloader

---
+++ modules/bootloaders/grub/icedos.nix
@@ -0,0 +1,37 @@
+{ icedosLib, ... }:
+
+{
+  options.icedos.hardware.bootloaders.grub.device = icedosLib.mkStrOption { default = ""; };
+
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        { config, ... }:
+        let
+          cfg = config.icedos;
+        in
+        {
+          boot = {
+            loader = {
+              grub = {
+                enable = true;
+                device = cfg.hardware.bootloaders.grub.device;
+                useOSProber = true;
+                enableCryptodisk = true;
+                configurationLimit = cfg.system.generations.bootEntries;
+              };
+
+              timeout = 1;
+            };
+
+            initrd.secrets = {
+              "/crypto_keyfile.bin" = null;
+            };
+          };
+        }
+      )
+    ];
+
+  meta.name = "grub";
+}
