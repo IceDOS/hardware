3d73c2746f5f5c4a38536d02765e38a2e6b76143
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-11 08:27:46 +0300

    feat(modukes): add deckbd driver

---
+++ modules/drivers/deckbd/icedos.nix
@@ -0,0 +1,55 @@
+{ ... }:
+
+{
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          pkgs,
+          ...
+        }:
+
+        let
+          deckbd = "${pkgs.deckbd}/bin/deckbd";
+        in
+        {
+          boot.initrd = {
+            preLVMCommands = ''
+              DECKBD_RETRIES=10
+              while true; do
+                ${deckbd} query && break
+                if [ "$DECKBD_RETRIES" -eq "0" ]; then break; fi
+                sleep 1
+
+                if [ "$DECKBD_RETRIES" -eq "1" ]; then
+                  echo -en "\rwaiting for deck controller to appear, $DECKBD_RETRIES retry remaining...  "
+                else
+                  echo -en "\rwaiting for deck controller to appear, $DECKBD_RETRIES retries remaining..."
+                fi
+                DECKBD_RETRIES=$((DECKBD_RETRIES - 1))
+              done
+
+              if [ ! "$DECKBD_RETRIES" -eq "0" ]; then
+                echo -en "\nstarting deckbd...\n"
+                ${deckbd} &
+                DECKBD_PID=$!
+              fi
+            '';
+
+            postMountCommands = ''
+              [ $DECKBD_PID ] && kill $DECKBD_PID
+            '';
+
+            kernelModules = [
+              "uinput"
+              "evdev"
+              "hid_steam"
+            ];
+          };
+        }
+      )
+    ];
+
+  meta.name = "deckbd";
+}
