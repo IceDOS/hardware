69251bef89af94d7ceac98af343948af052c11f5
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-11 14:59:46 +0300

    feat(modules): add kernel

---
+++ modules/kernel/icedos.nix
@@ -0,0 +1,55 @@
+{ icedosLib, ... }:
+
+{
+  options.icedos.hardware.kernel =
+    let
+      inherit (icedosLib) mkNumberOption mkStrOption;
+    in
+    {
+      swappiness = mkNumberOption { default = 1; };
+      version = mkStrOption { default = "lts"; };
+    };
+
+  outputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          pkgs,
+          ...
+        }:
+
+        let
+          cfg = config.icedos;
+          kernel = cfg.hardware.kernel;
+
+          chaoticKernel =
+            kernel == "cachyos" || kernel == "cachyos-rc" || kernel == "cachyos-server" || kernel == "valve";
+        in
+        {
+          boot = {
+            kernelPackages =
+              with pkgs;
+              if (cfg.internals.isFirstBuild && chaoticKernel) then
+                linuxPackages
+              else
+                {
+                  cachyos = linuxPackages_cachyos;
+                  cachyos-server = linuxPackages_cachyos-server;
+                  cachyos-rc = linuxPackages_cachyos-rc;
+                  latest = linuxPackages_latest;
+                  lts = linuxPackages;
+                  valve = linuxPackages_jovian;
+                  zen = linuxPackages_zen;
+                }
+                .${kernel.version};
+
+            kernel.sysctl."vm.swappiness" = toString (kernel.swappiness);
+          };
+        }
+      )
+    ];
+
+  meta.name = "kernel";
+}
