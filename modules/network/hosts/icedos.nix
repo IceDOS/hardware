f2d145023be705f222567a65ee9f44e8aa3316b9
IceDBorn <git.outsider841@simplelogin.fr>
2025-08-12 07:48:23 +0300

    feat(modules): add network hosts

---
+++ modules/network/hosts/icedos.nix
@@ -0,0 +1,24 @@
+{ icedosLib, ... }:
+
+{
+  options.icedos.hardware.network.hosts = icedosLib.mkLinesOption { default = ""; };
+
+  ouputs.nixosModules =
+    { ... }:
+    [
+      (
+        {
+          config,
+          ...
+        }:
+
+        {
+          networking = {
+            extraHosts = config.icedos.hardware.network.hosts;
+          };
+        }
+      )
+    ];
+
+  meta.name = "hosts";
+}
