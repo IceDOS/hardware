7e36b2c37b6b429914145804f7e9bbc95d8e057d
IceDBorn <git.outsider841@simplelogin.fr>
2025-12-13 02:31:58 +0200

    fix(uinput): add group to users

--- modules/uinput/icedos.nix
+++ modules/uinput/icedos.nix
@@ -4,9 +4,19 @@
   outputs.nixosModules =
     { ... }:
     [
-      {
-        hardware.uinput.enable = true;
-      }
+      (
+        { config, lib, ... }:
+
+        let
+          inherit (config.icedos) users;
+          inherit (lib) mapAttrs;
+        in
+        {
+          hardware.uinput.enable = true;
+
+          users.users = mapAttrs (_: _: { extraGroups = [ "uinput" ]; }) users;
+        }
+      )
     ];
 
   meta.name = "uinput";
