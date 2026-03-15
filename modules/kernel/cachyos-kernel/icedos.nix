f2ef3525a43ed550e34703167f00f27c6c95ebda
IceDBorn <git.outsider841@simplelogin.fr>
2026-03-15 21:05:26 +0200

    rework(cachyos-kernel): use cachyos zfs package when zfs is enabled

--- modules/kernel/cachyos-kernel/icedos.nix
+++ modules/kernel/cachyos-kernel/icedos.nix
@@ -15,26 +15,35 @@
         }:
 
         let
-          inherit (config.icedos.hardware.kernel) variant;
+          inherit (config) boot icedos;
+          inherit (boot) kernelPackages supportedFilesystems;
+          inherit (kernelPackages) zfs_cachyos;
+          inherit (supportedFilesystems) zfs;
+          inherit (icedos.hardware.kernel) variant;
           inherit (inputs) icedos-state nix-cachyos-kernel;
-          inherit (lib) elem importJSON mkForce;
-          inherit (nix-cachyos-kernel.overlays) pinned;
+
+          inherit (lib)
+            elem
+            importJSON
+            mkForce
+            mkIf
+            ;
+
+          inherit (nix-cachyos-kernel.overlays) default;
           inherit (pkgs) cachyosKernels linuxPackages;
 
           substituter = "https://attic.xuyh0120.win/lantian";
+
+          hasSubstituter = elem substituter (
+            if (icedos-state != null) then importJSON "${icedos-state}/substituters" else [ ]
+          );
         in
         {
           boot.kernelPackages =
-            if
-              (elem substituter (
-                if (icedos-state != null) then importJSON "${icedos-state}/substituters" else [ ]
-              ))
-            then
-              mkForce cachyosKernels."linuxPackages-cachyos-${variant}"
-            else
-              linuxPackages;
+            if hasSubstituter then mkForce cachyosKernels."linuxPackages-cachyos-${variant}" else linuxPackages;
 
-          nixpkgs.overlays = [ pinned ];
+          boot.zfs.package = mkIf (hasSubstituter && zfs) zfs_cachyos;
+          nixpkgs.overlays = [ default ];
           nix.settings.substituters = [ substituter ];
           nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
         }
